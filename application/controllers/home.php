e50c40b86e45a79836b8b8cb326551012268c1db
OmniaCS <OmniaCS>
2015-06-17 00:28:25 +0200

    All CI files
    Check them all

---
+++ application/controllers/home.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		ob_start();
+		 $this->load->database();
+        $this->load->helper('url');
+		$this->load->library('grocery_CRUD');
+	}
+	function _order_output($output = null)
+	{
+		$this->load->view('add_order_view',$output);
+	}/*
+	function _output($html = null)
+	{
+		echo $html;
+	}
+*/
+
+	public function add_order()
+	{
+		//echo "hi user";
+		$crud = new grocery_CRUD();
+		$crud->set_theme('Flexigrid');
+		$crud->set_subject('New Order');
+		$crud->set_table('orders');
+	/*	$crud->fields('item_id','item_quantity','main_item_id','order_start_date','order_status');
+		$crud->set_rules('item_id','Main Drink','required')->display_as('item_id','Main Drink')
+			 ->set_rules('item_quantity','Item Quantity','required')->display_as('item_quantity','Item Quantity')
+			 ->set_rules('main_item_id','','required')->display_as('main_item_id','Main');
+	*/
+		$output = $crud->render();
+		$this->_order_output($output);
+	}
+	public function admin_panel()
+	{
+		echo "hi admin";
+	}
+}
+
---
+++ application/views/home_page.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>CAFE</title>
+	</head>
+	<body>
+		<a href="<?php echo base_url('home/add_order')?>">Users</a>
+		<a href="<?php echo base_url('home/admin_panel')?>">Admin</a>
+	</body>
+</html>
