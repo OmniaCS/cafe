a5b4e54b77153bd01c9d724e4692d24231cc452a
OmniaCS <OmniaCS>
2015-06-18 05:29:14 +0200

    Add new order

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -9,6 +9,7 @@ class Home extends CI_Controller {
 		 $this->load->database();
         $this->load->helper('url');
 		$this->load->library('grocery_CRUD');
+		$this->load->model('grocery_CRUD_Model');
 	}
 	function _order_output($output = null)
 	{
@@ -22,16 +23,15 @@ class Home extends CI_Controller {
 
 	public function add_order()
 	{
-		//echo "hi user";
 		$crud = new grocery_CRUD();
 		$crud->set_theme('Flexigrid');
 		$crud->set_subject('New Order');
 		$crud->set_table('orders');
-	/*	$crud->fields('item_id','item_quantity','main_item_id','order_start_date','order_status');
+		$crud->fields('item_id','item_quantity','main_item_id');//,'order_start_date --> will be inserted automatically and order_status--> will be inserted from office boy;
 		$crud->set_rules('item_id','Main Drink','required')->display_as('item_id','Main Drink')
-			 ->set_rules('item_quantity','Item Quantity','required')->display_as('item_quantity','Item Quantity')
-			 ->set_rules('main_item_id','','required')->display_as('main_item_id','Main');
-	*/
+			 ->set_rules('item_quantity','Item Quantity','required')->display_as('item_quantity','Item Quantity');
+		//	 ->set_rules('main_item_id','','required')->display_as('main_item_id','Main');
+	
 		$output = $crud->render();
 		$this->_order_output($output);
 	}
